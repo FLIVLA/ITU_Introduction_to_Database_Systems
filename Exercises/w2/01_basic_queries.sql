1c86a9b8b1dc129c87b74eac3969d0bad3d13c29
Frederik Lind <fred2193@stud.kea.dk>
2023-02-16 03:07:23 +0100

    wpi w2

--- Exercises/w2/01_basic_queries.sql
+++ Exercises/w2/01_basic_queries.sql
@@ -97,13 +97,120 @@ their best result. The second-to-last column should be named “best” while
 the last column should be named “difference”. The last column should
 always contain non-negative values and should preferably be formatted to
 show at least one digit before the decimal point and exactly two digits after
-the decimal point.
-This query requires a three-way JOIN, along with WHERE, GROUP BY and
-HAVING. You can use the PostgreSQL-specific code to_char(S.record-
-max(R.result), '0D99') to format the text. This query should return 7
-rows. Why is it that, if you do no formatting, the difference has so many digits? */
+the decimal point. */
 
 -- SOLUTION:
 SELECT people.id, people.name 
 FROM people
 INNER JOIN 
+
+--------------------------------------------------------------------------------
+/* 8. The ID, name and gender description of all athletes who participated in the
+competition held in Hvide Sande in 2009.
+You will need to join four tables to find the result here. You can use the
+PostgreSQL-specific code extract(year from C.held) to find the year. This
+query should return 84 rows. */
+
+-- SOLUTION:
+SELECT DISTINCT(people.id), people.name, gender.description
+FROM people
+INNER JOIN gender 
+ON gender.gender = people.gender
+INNER JOIN results
+ON results.peopleid = people.id
+INNER JOIN competitions
+ON competitions.id = results.competitionid
+WHERE competitions.place = 'Hvide Sande'
+AND EXTRACT(YEAR FROM competitions.held) = 2009 -- returns 84 rows.
+
+-- NOTES:
+-- "EXTRACT(YEAR FROM 'clm_name')" - postgreSQL specific syntax
+
+--------------------------------------------------------------------------------
+/* 9. The name and gender description of all people with a last name that starts
+with a “J” and ends with a “sen” (e.g., Jensen, Jansen, Johansen).
+This query should return 82 rows. */
+
+-- SOLUTION:
+SELECT people.name, gender.description
+FROM people
+INNER JOIN gender
+ON gender.gender = people.gender
+WHERE SPLIT_PART(people.name, ' ', 2) LIKE 'J%sen' -- returns 82 rows
+
+/* NOTES:
+ - "SPLIT_PART(clm_name, 'delimiter', item_position)" - SQL string split 
+ - ILIKE can be used to make a case-sensitive comparison */
+
+--------------------------------------------------------------------------------
+/* 10. For each result, the name of the athlete, the name of the sport, and the
+percentage of the record achieved by the result (a result that is a record
+should therefore appear as 100; this column should be named
+“percentage”). Preferably, format the last column to show only whole
+numbers, as well as the % sign, you can use CASE to detect when the result
+is NULL and when not. */
+
+-- SOLUTION:
+SELECT people.name, sports.name,
+    CONCAT(
+		CEILING((COALESCE(results.result, 0)/sports.record) * 100), '%'
+	) AS percentage  
+FROM results
+INNER JOIN people
+ON people.id = results.peopleid
+INNER JOIN sports
+ON sports.id = results.sportid
+
+/* NOTES:
+ - COALESCE can be used to replace all null values.
+ - CEILING is used to roud up to nearest integer.
+ - CONCAT is used to format the calculated column,
+   in this case to place the '%' behind the percentage value. */  
+
+--------------------------------------------------------------------------------
+/* 11. The number of athletes with some incomplete result registrations.
+This single-table query should return a single row with a single column: the
+number 75. */
+
+-- SOLUTION:
+SELECT COUNT(DISTINCT people.id)
+FROM people
+INNER JOIN results
+ON results.peopleid = people.id
+WHERE results.result IS NULL
+
+--------------------------------------------------------------------------------
+/* 12. For each sport, show the ID and name of the sport and the best
+performance over all athletes and competitions. This last column should be
+called ‘maxres’ and should be formatted to show at least one digit before
+the decimal point and exactly two digits after the decimal point. The query
+result should be ordered by the sport ID. */
+
+-- SOLUTION:
+SELECT sports.id, sports.name, CAST(MAX(results.result) AS DECIMAL(10,2)) AS maxres
+FROM sports
+INNER JOIN results
+ON results.sportid = sports.id
+GROUP BY sports.id, sports.name
+ORDER BY sports.id ASC
+
+--------------------------------------------------------------------------------
+/* 13. Show the ID and name of athletes who hold a record in at least two sports,
+along with the total number of their record-setting or record-equaling
+results.
+This query requires joining three tables, along with WHERE, GROUP BY and
+HAVING clauses. It should return a single row: (25, Jens Jansen, 21). */
+
+-- SOLUTION:
+SELECT people.id, people.name
+FROM people
+INNER JOIN results
+ON results.peopleid = people.id
+INNER JOIN sports
+ON sports.id = results.sportid
+WHERE results.result = sports.record
+GROUP BY people.id
+HAVING count(*) >= 2
+
+-- THIS RETURNS 3 COLUMNS? 
+-- ALSO CONFUSED ABOUT CLM3 VALUE?
